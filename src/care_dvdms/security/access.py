5d47415b5e22af0e0ed83dce2ac612f3884721f6
pranavrutagi-egov <pranav.rutagi@egovernments.org>
2026-08-12 16:26:39 +0530

    Implement CareDVDMS access and permissions for DVDMS integration

--- src/care_dvdms/security/access.py
+++ src/care_dvdms/security/access.py
@@ -5,7 +5,23 @@ from care_dvdms.security.permissions import CareDVDMSPermissions
 
 
 class CareDVDMSAccess(AuthorizationHandler):
-    pass
+    """
+    Check if the user has permission to use DVDMS integration in the facility
+    """
+
+    def can_use_dvdms_integration(self, user, facility):
+        return self.check_permission_in_facility_organization(
+            [CareDVDMSPermissions.can_use_dvdms_integration.name],
+            user,
+            facility=facility,
+        )
+
+    def can_manage_dvdms_integration(self, user, facility):
+        return self.check_permission_in_facility_organization(
+            [CareDVDMSPermissions.can_manage_dvdms_integration.name],
+            user,
+            facility=facility,
+        )
 
 
 AuthorizationController.register_internal_controller(CareDVDMSAccess)
